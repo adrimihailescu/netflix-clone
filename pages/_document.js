0250213a0a5ca1dcf84d8f26ebc63e1564c41475
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-12 17:11:50 +0100

    setup project,fonts and basic style

---
+++ pages/_document.js
@@ -0,0 +1,34 @@
+/* eslint-disable react/jsx-no-undef */
+import Document, {
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/dist/pages/_document";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main></Main>
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
