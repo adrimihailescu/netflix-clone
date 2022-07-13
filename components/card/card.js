a7f1d5ce287e5ba2a7e9783a816fa741aeda8110
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-13 11:32:28 +0100

    add Card component size

--- components/card/card.js
+++ components/card/card.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Image from "next/image";
+import styles from "./card.module.css";
 
 const Card = (props) => {
 	const { imgUrl, size } = props;
+
+	//configure specifix size
+	const classMap = {
+		large: styles.lgItem,
+		medium: styles.mdItem,
+		small: styles.smItem,
+	};
+
 	return (
-		<div>
+		<div className={styles.container}>
 			Card
-			<Image src={imgUrl} alt="image" width="300px" height="300px" />
+			<div className={classMap[size]}>
+				<Image
+					src={imgUrl}
+					alt="image"
+					layout="fill"
+					className={styles.cardImg}
+				/>
+			</div>
 		</div>
 	);
 };
