3e4b325d486184c7463ec8d4b745d2a533bd5adb
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-13 10:43:22 +0100

    add NavBar router

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -2,8 +2,23 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
 
+import { useRouter } from "next/router";
+
 const NavBar = (props) => {
 	const { username } = props;
+
+	const router = useRouter();
+
+	const handleOnClickHome = (e) => {
+		e.preventDefault();
+		router.push("/");
+	};
+
+	const handleOnClickMyList = (e) => {
+		e.preventDefault();
+		router.push("/browser/my-list");
+	};
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.wrapper}>
@@ -11,8 +26,12 @@ const NavBar = (props) => {
 					<div className={styles.logoWrapper}>NETFLIX</div>
 				</Link>
 				<ul className={styles.navItems}>
-					<li className={styles.navItem}>Home</li>
-					<li className={styles.navItem2}>My List</li>
+					<li className={styles.navItem} onClick={handleOnClickHome}>
+						Home
+					</li>
+					<li className={styles.navItem2} onClick={handleOnClickMyList}>
+						My List
+					</li>
 				</ul>
 				<nav className={styles.navContainer}>
 					<div>
@@ -21,7 +40,9 @@ const NavBar = (props) => {
 						</button>
 						<div className={styles.navDropdown}>
 							<div>
-								<a className={styles.linkName}>Sign out</a>
+								<Link href="/login">
+									<a className={styles.linkName}>Sign out</a>
+								</Link>
 								<div className={styles.lineWrapper}></div>
 							</div>
 						</div>
