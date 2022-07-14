2cf6a21c3d7f806056d77cede02d1a77b590a0cc
adriana mihalescu <adriana.mihalescu@gmail.com>
2022-07-14 17:18:34 +0100

    add react modal, modal component and implement API youtube player and styling

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,10 +1,34 @@
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+
+import styles from "../../styles/Video.module.css";
+
+Modal.setAppElement("#__next");
 
 //dynamic route
 const Video = () => {
 	const router = useRouter();
-	console.log({ router });
-	return <div>Video {router.query.videoId}</div>;
+	return (
+		<div className={styles.container}>
+			<Modal
+				isOpen={true}
+				contentLabel="Watch the video"
+				onRequestClose={() => router.back()}
+				overlayClassName={styles.overlay}
+				className={styles.modal}
+			>
+				<iframe
+					id="ytplayer"
+					type="text/html"
+					className={styles.videoPlayer}
+					width="100%"
+					height="360"
+					src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
+					frameBorder="0"
+				></iframe>
+			</Modal>
+		</div>
+	);
 };
 
 export default Video;
